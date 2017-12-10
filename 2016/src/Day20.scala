218601d72ddec76fedca1ac251f9f395ea9ccfec
Rodrigo Marcuschi <rodrigo.marcuschi@C1MT10M3H3QK.workdayinternal.com>
2017-12-10 19:39:57 +0000

    Added Day 20 for 2016, Days 6/7 for 2017

---
+++ 2016/src/Day20.scala
@@ -0,0 +1,27 @@
+import scala.io.Source
+
+object Day10 {
+
+  def main(args: Array[String]): Unit = {
+    val lines = Source
+      .fromFile("inputs/2016/input_day10.txt")
+      .getLines
+      .toList
+      .map(_.split("-"))
+      .map(arr => (arr(0).toLong, arr(1).toLong))
+      .sorted
+      .foldLeft(List[(Long, Long)]()) { (acc, t) =>
+      acc match {
+        case x :: xs if x._2 >= t._1 => (x._1, math.max(x._2, t._2)) :: xs
+        case x :: xs => t +: acc
+        case _ => List(t)
+      }
+    }.reverse
+
+
+    // getFirstAllowed(lines)
+    println(lines.sliding(2).map(k => (k.head._2, k.last._1)).find(p => p._2 - p._1 > 1).get._1 + 1)
+    println(lines.sliding(2).map(k => k.last._1 - k.head._2 - 1).sum)
+  }
+
+}
